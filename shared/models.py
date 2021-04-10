a27a25d40d4b92dbe21353ad06d7e64bad5fcd50
Simon Machera <44668989+CymonMachera@users.noreply.github.com>
2021-04-10 11:12:06 +0300

    Rectified the shared API

--- shared/models.py
+++ shared/models.py
@@ -7,7 +7,10 @@ class SharedFolder(models.Model):
     shared_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     shared_to = models.ManyToManyField(CustomUser, related_name = "shared_folder")
     date_shared = models.DateTimeField(auto_now_add=True)
-    folder_link  = models.CharField(max_length = 50, blank = False, null = False, default = None)
+    folder_link  = models.CharField(max_length = 100, blank = False, null = False, default = None)
+    pillar_id  = models.CharField(max_length = 2, blank = False, null = False, default = None)
+    program_id  = models.CharField(max_length = 20, blank = False, null = False, default = None)
+    activity_id  = models.CharField(max_length = 20, blank = False, null = False, default = None)
 
 
     def __str__(self):
@@ -17,7 +20,7 @@ class SharedFile(models.Model):
     shared_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     shared_to = models.ManyToManyField(CustomUser, related_name = "shared_file")
     date_shared = models.DateTimeField(auto_now_add=True)
-    file_link  = models.CharField(max_length = 50, blank = False, null = False, default = None)
+    file_link  = models.CharField(max_length = 100, blank = False, null = False, default = None)
 
     def __str__(self):
         return self.file_link

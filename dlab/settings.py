66392434fc2627102d68117996f9a43b35fc3c53
cymonmachera <cymonmachera1@gmail.com>
2021-05-21 17:20:01 +0300

    static file rectified

--- dlab/settings.py
+++ dlab/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 with open('./secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 

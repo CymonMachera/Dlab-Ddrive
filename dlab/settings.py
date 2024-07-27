7d78091e06df375b17b568051eefbf7e53842167
cymonmachera <cymonmachera@gmail.com>
2024-07-27 19:29:03 +0300

    remove unnecesary files

--- dlab/settings.py
+++ dlab/settings.py
@@ -103,11 +103,8 @@ WSGI_APPLICATION = 'dlab.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dlab',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
